7dcab0ade70a2a75bf56cd58e05523ddd8348559
Obakinyemi <beteiscoo@gmail.com>
2025-06-13 22:33:23 -0600

    I now know how to use environment variables with javascript.

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const fs = require('fs')
 
-// import * as osu from "osu-api-v2-js"
+import * as osu from "osu-api-v2-js"
+
+require('dotenv').config();
+const port = process.env.PORT;
+console.log(`The port is ${port}`);
+id = process.env.client_id;
+secret = process.env.client_secret;
 
 
-
-fs.readFile('test.txt', 'utf-8', (err, data) => {
-    if(err){
-        console.error(err);
-        return;
-    }
-    console.log(data);
-});
+// fs.readFile('test.txt', 'utf-8', (err, data) => {
+//     if(err){
+//         console.error(err);
+//         return;
+//     }
+//     console.log(data);
+// });
 
 // fs.writeFile(
 //     'test.txt',
@@ -21,3 +26,12 @@ fs.readFile('test.txt', 'utf-8', (err, data) => {
 //         return;
 //     }
 // });
+// c_id = getenv("c_id");
+// console.log(c_id);
+// async function getTopPlays(username) {
+//     try {
+//         const api = await osu.API.createAsync("41606", "LGRrogtg3SdChaIqGkrTwCvV3bK7xvX0VjIsPt0Y")
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
