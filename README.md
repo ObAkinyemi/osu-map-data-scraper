350b42d7f264bec9a8ac88a685bc941672244e47
Obakinyemi <beteiscoo@gmail.com>
2025-06-14 12:18:21 -0600

    added type module stuff to package.json file and name: file name to package.json. Used fetch of base url instead of syntax given in git repo for osu api.

---
+++ README.md
@@ -0,0 +1,5 @@
+I ran into the problem of figuring out how to import stuff in es6 using require instead of import.
+I am meant to use import x from 'y' or import 'a/b'.
+this: https://www.geeksforgeeks.org/javascript/javascript-program-to-read-text-file/
+to this: https://www.npmjs.com/package/dotenv#%EF%B8%8F-usage
+https://www.youtube.com/watch?v=QHb75wm2iLk
--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-const fs = require('fs')
+// import fs from 'fs'
+
 
 import * as osu from "osu-api-v2-js"
 
-require('dotenv').config();
-const port = process.env.PORT;
-console.log(`The port is ${port}`);
-id = process.env.client_id;
-secret = process.env.client_secret;
+import dotenv from 'dotenv';
+dotenv.config();
+console.log(process.env)
 
+
+const id = process.env.CLIENT_ID;
+const secret = process.env.CLIENT_SECRET;
+console.log(id);
+console.log(secret);
 
 // fs.readFile('test.txt', 'utf-8', (err, data) => {
 //     if(err){
@@ -26,12 +30,19 @@ secret = process.env.client_secret;
 //         return;
 //     }
 // });
-// c_id = getenv("c_id");
-// console.log(c_id);
-// async function getTopPlays(username) {
-//     try {
-//         const api = await osu.API.createAsync("41606", "LGRrogtg3SdChaIqGkrTwCvV3bK7xvX0VjIsPt0Y")
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }
+
+async function logUserTopPlayBeatmap() {
+    // It's more convenient to use `osu.API.createAsync()` instead of `new osu.API()` as it doesn't require you to directly provide an access_token!
+    // In a proper application, you'd use this function as soon as the app starts so you can use that object everywhere
+    // (or if it acts as a user, you'd use this function at the end of the authorization flow)
+    try {
+        const api = await fetch(`https://osu.ppy.sh/api/v2/`);// with id as a number
+
+        console.log(api);
+        // Doomsday fanboy's top play is on: Erio o Kamattechan - os-Uchuujin(Asterisk Makina Remix) [Mattress Actress] +DT,CL (8.87*)
+    } catch(err) {
+        console.error(err);
+    }
+}
+
+logUserTopPlayBeatmap();
