a0f75f4e32e74ab7cc6157e4bfbdcac2db8a86cc
Obakinyemi <beteiscoo@gmail.com>
2025-07-18 00:25:24 -0500

    I am getting the individual beatmap data into the json file. I have tried using maps, but I don't know how to get the syntax working properly.It's probably better if I get a map of arrays rather than an array of maps or more accurately, a map of maps. On second thought. I'm just gonna keep it as a nch of strings.

--- script.js
+++ script.js
@@ -103,13 +103,15 @@ async function getBeatmapSetData(beatmapsetID, filename){
 			for (let id_num of beatmapsetID){
 				const setData = await api.getBeatmapset(id_num);
 				let beatmapIDArray = [];
+				let bmIDArray = new Map(); 
 
 				for (let i in setData.beatmaps){
 					// console.log(i);
-					beatmapIDArray.push(setData.beatmaps[i].id);
+					// beatmapIDArray.push(`{"beatmap id": ${setData.beatmaps[i].id}, "beatmap difficulty rating": ${setData.beatmaps[i].difficulty_rating}, "beatmap total length": ${setData.beatmaps[i].total_length}, "beatmap bpm": ${setData.beatmaps[i].bpm}}`);
+					beatmapIDArray.push(bmIDArray.set('beatmap id', setData.beatmaps[i].id)); 
 				}
 				// gives beatmap info and not beatmap array info.
-				console.log(beatmapIDArray);
+				console.log(bmIDArray);
 				fs.appendFile(filename, `{\n"beatmapset id": "${setData.id}",\n	"beatmap ids": [${beatmapIDArray}]\n},\n`,
 				(err) => err && console.error(err));
 				// testWrite(filename, `${setData.title}:\n ${JSON.stringify(setData)}`);
